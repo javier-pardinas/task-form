53fecbd9a4bd2412e4e3dc967e20e33c3a0d77c2
javier-pardinas <javierpb275@gmail.com>
2021-05-04 14:12:21 +0200

    mostrado formulario de strapi haciendo un fetch del y cogiendo los datos y mostrandolos en el componente

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,56 +1,24 @@
 <template>
-<div class="formulario">
-    <h1 class="title">{{ title }}</h1>
-    <p class="order">Orden: {{ order }}</p>
-    <p class="date">Fecha ultima revision: {{ date }}</p>
-    <p class="resume">Resumen: {{ resume }}</p>
-    <p class="text">Texto: {{ text }}</p>
+  <div class="formulario">
+    <!--we loop through those posts that we get from our api and display their info:-->
+    <div v-for='form in forms' :key='form.id'>
+        <h1>{{form.titulo}}</h1>  
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
-export default{
-    name: 'Formulario',
-    components: {
-    },
-    data(){
-        return{
-            all: null,
-            title: null,
-            order: null,
-            date: null,
-            resume: null,
-            text: null,
-            API: 'https://externo-smartlex-api-contenidos.iberley.es/',
-            id: 'formularios/70521/{texto}',
-            token: '?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6InRlc3QifQ.eyJ1c2VybmFtZSI6Im1jYXJiYWxsb0BpYmVybGV5LmVzIiwiaWQiOiI3MCIsImV4cCI6MTYxODk4NjI4NywiaWF0IjoxNjE4ODI0Mjg3LCJyb2xlcyI6WyJST0xFX1BSRU1JVU0iXSwibWF0IjoiUEUsQ0ksTUUsQURNLEZJLExBIiwiY29sIjoiQ0MsU1VCLExFLENWIiwiZ2MiOiIyIn0.mZ2TlHcpOTlmcsAnINTGJVTextxnWZm9nDk1bVWLWm5jfH0uHuC-uLMN9ruP9N-W0pg6hBY4CQ8iQZ01OXxragYqH-L0WP3M2f_LbVrcG-Bu0TPckU1Wi-ONw-PCU9XXnZzEt-YLHFTNGekPp60crvF5Jb-e3ramqCsrLV0m8l9VUU9tl-vR450x0YcNrJT44o67pvs0-fYBYlfgPWaVZg-pLhWsGrDKi0AVPgUx2g_2NdzavNEHc6wWhzZ90fN13Y2vH6UeQFx49PH237CfbZuBbGt50FxZjTKYBs4xyvYfuPY1ZbsiSciDCrkWZG-4gLRly9WY0CB2BGtJLwV4-FalxISonr9Y4Q286Bwj5MY3mFE6VYi13HffEnUXe47OcfbHnqGjf5VmMsAHBBM3eG5n7AZVbSKOX2esjTftDOgy4fCoPqdEzgElZtna57e5rNGMx8AKj65qX0inpQ949NbeJJD9k7e4MlHFEB1VyxRUZg1RafbESktYdk8S03GIKPLuch1cBjAlABP_BSryuK7ZfkvRpRyVMrzK4ELJbm6vLAcaLKIOVt8iW3DVSK6zJihuTCP_BmfX2kOXn1fLs94FKdW_d8haCIpP6AmNWNCDikAmJwXdNVhK96_JinBYUulaJj41DOf03trzA-LYAP92H-JDqs3sm94E2NCiDTY&_format=json',
-        }
-    },
-
-    mounted(){
-        this.getNew();
-    },
-
-    methods: {
-        getNew(){
-            axios.get(this.API + this.id + this.token)
-            .then (response =>{
-                this.all = response.data,
-                console.log(this.all),
-                this.title = response.data.titulo,
-                console.log(this.titulo),
-                this.date = response.data.seofechamodificacion_desde
-                console.log(this.date),
-                this.resume = response.data.resume
-                console.log(this.resume),
-                this.text = response.data.text
-                console.log(this.text)
-            })
-            .catch(e => console.log(e))
-        }
+export default {
+  name: 'FetchPosts',
+  computed: {
+    //forms from the state of store:
+    forms() {
+    return this.$store.state.forms
     }
+  },
+  mounted() {
+    //dispatch the action fetchForms() that gets forms from strapi and commits the set_forms() mutation:
+    this.$store.dispatch("fetchForms");
+  }
 }
-
-</script>
+</script> 
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    forms: []
   },
   mutations: {
+    //set the posts array with the data that we get from the api using the action getPosts()
+    SET_FORMS(state, forms) {
+      state.forms = forms
+    }
   },
   actions: {
+  //fetch data from strapi and set the forms array with the data by commiting the SET_FORMS() mutation
+    fetchForms({commit}) {
+      // GET request using fetch with error handling
+      fetch('http://localhost:8000/Formularios')
+      .then(async response => {
+        const data = await response.json();
+        // check for error response
+        if (!response.ok) {
+          // get error message from body or default to response statusText
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+        commit('SET_FORMS', data)
+      })
+      .catch(error => {
+        this.errorMessage = error;
+        console.error("There was an error!", error);
+      });
+    }
+    
   },
   modules: {
   }
