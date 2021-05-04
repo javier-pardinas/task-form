37789c3f3e9e6ab82b9c267d7b9a5c9a860a1b9e
javier-pardinas <javierpb275@gmail.com>
2021-05-04 12:15:10 +0200

    apollo setup

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import store from './store'
 
 //PLUGINS:
 import './plugins'
+import VueApollo from "vue-apollo";
+import apolloClient from "./vue-apollo"
 
 Vue.config.productionTip = false
 
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
 new Vue({
+  apolloProvider,
   router,
   store,
   render: h => h(App)
